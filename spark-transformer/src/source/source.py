0da322e8f4d6c5f4f41f2ed2ea721859472a891f
Hammad KHAN <hammad.khan@mangabey.io>
2023-11-01 08:32:28 +0100

    delete unused folders

--- spark-transformer/src/source/source.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Source(ABC):
-    def __init__(self, config):
-        self.name = config['name']
-    
-    @abstractmethod
-    def read(self, spark):
-        pass
